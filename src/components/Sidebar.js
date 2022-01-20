3d4e1198f29de2b744977d7b74728042369120b7
Phil Than <82873669+pthan1@users.noreply.github.com>
2022-01-19 23:48:33 -0700

    Form error handling on login feature so first value doesn't trigger fetch

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -5,8 +5,11 @@ const Sidebar = ({users, currentUser, setCurrentUser, userConversations, setUser
   const userOptions = users.map(user => {return <option value={user.id}>{user.name}</option>})
 
   const handleLogin = (e) => {
+    if (e.target.value) {
+    const userId = Number(e.target.value)
+    setCurrentUser(userId);
       //on user select, get all of that user's convos and display the other preson in that convo
-      fetch(`http://localhost:3001/api/v1/conversations/${e.target.value}`)
+      fetch(`http://localhost:3001/api/v1/conversations/${userId}`)
       .then(response => {
         if (!response.ok) {
          throw new Error(`Status: ${response.status}`)
@@ -14,14 +17,14 @@ const Sidebar = ({users, currentUser, setCurrentUser, userConversations, setUser
         return response.json()
       })
       .then(data => setUserConversations(data.conversations))
-      .catch(error => console.log(error))
+      .catch(error => console.log(error))}
   }
 
 
   return (
     <div className="sidebar">
       <select name="userOptions" id="userOptions" onChange={handleLogin}>
-       <option>Login</option> 
+       <option value="">Login</option> 
             {userOptions}</select><br />
     </div>
   )
