a01fb4f5ae29a2c07d8a5236e89f7dcefddcbace
Phil Than <82873669+pthan1@users.noreply.github.com>
2022-01-20 01:00:51 -0700

    Set index of the conversation in order to access it and it's messages on click

--- src/components/Conversations.js
+++ src/components/Conversations.js
@@ -2,24 +2,29 @@
 const Conversations = ({userConversations, currentUser, users}) => {
 
   const partners = () => {
-    const partnerIds = userConversations.map(conversation => {
+    const partnerIds = userConversations.map((conversation, index) => {
       const ids = conversation.user_ids.filter(element => element !== currentUser);
-      return ids;
+      return {index, ids};
     })
     console.log('partnerIds', partnerIds)
 
-    let convoPartners = partnerIds.map(id => 
-      users.find(user => {
-        return user.id == id;
+    let convoPartners = partnerIds.map(id => {
+      const partner = users.find(user => {
+        return user.id === id.ids[0];
         }
       )
-    )
-    convoPartners = convoPartners.map(partner => <button>{partner.name}</button>)
+      return {partner: partner, id: id.index}
+      })
+
+    console.log(convoPartners)
+    convoPartners = convoPartners.map(partner => <button onClick={handleClick}>{partner.partner.name}</button>)
 
     return convoPartners;
   }
-    
-    
+    const handleClick = () => {
+console.log('click')
+    }
+
 
   return (
     <div className="partners">
