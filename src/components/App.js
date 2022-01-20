8524607486ef86ab3f2da3bf116e033fc6be2bef
Phil Than <82873669+pthan1@users.noreply.github.com>
2022-01-19 22:58:37 -0700

    Clean up project

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="App">
+    {/* sidebar component */}
+   {/* Conditional Render:
+        if no user signed in, prompt a message to sign in
+        //if a user is signed in, Add a welcome message
+        //prompt user to choose a conversation
+         */}
+    {/* Right side component */}
+
+    </div>
+  );
+}
+
+export default App;
